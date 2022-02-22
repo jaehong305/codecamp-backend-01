d3bb0b53babd1d1cd49c4db2aeec7c4673aa8a13
ljh305 <ljh3057713@gmail.com>
2022-02-22 17:18:31 +0900

    fix db

--- class/28-01-deploy-with-docker/backend/src/app.module.ts
+++ class/28-01-deploy-with-docker/backend/src/app.module.ts
@@ -30,7 +30,7 @@ import { FileModule } from './apis/file/file.module';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'my_database',
       port: 3306,
       username: 'root',
       password: '159159',
