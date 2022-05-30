94aaa62b413b07e7f5a134fe1caee71e92b44b83
jaehong305 <jaehong30577@gmail.com>
2022-05-30 17:18:01 +0900

    review

--- class/12-03-typescript/index.ts
+++ class/12-03-typescript/index.ts
@@ -1,16 +1,16 @@
 // 문자타입 - 타입추론
-let aaa = '안녕하세요';
-aaa = '반갑습니다';
+let aaa = "안녕하세요";
+aaa = "반갑습니다";
 aaa = 3;
 
 // 문자타입
-let bbb: string = '반갑습니다';
-bbb = '반가워요!!';
+let bbb: string = "반갑습니다";
+bbb = "반가워요!!";
 bbb = 123;
 
 // 숫자타입
 let ccc: number = 5;
-ccc = '33';
+ccc = "33";
 
 // 불린타입
 let ddd: boolean = true;
@@ -19,11 +19,11 @@ ddd = false;
 
 // 배열타입
 let eee: number[] = [1, 2, 3, 4, 5];
-let fff: string[] = ['철수', '영희', '훈이'];
-let ggg: (number | string)[] = [1, '철수', 2, '영희'];
+let fff: string[] = ["철수", "영희", "훈이"];
+let ggg: (number | string)[] = [1, "철수", 2, "영희"];
 
 let mymoney: number[] | string[] = [1000, 2000, 3000];
-mymoney = ['1000원', '2000원', '3000원'];
+mymoney = ["1000원", "2000원", "3000원"];
 
 // 객체타입
 interface IProfile {
@@ -33,11 +33,11 @@ interface IProfile {
 }
 
 let profile: IProfile = {
-  name: '철수',
+  name: "철수",
   age: 13,
-  school: '다람쥐초등학교',
+  school: "다람쥐초등학교",
 };
-profile.age = '13살';
+profile.age = "13살";
 
 // 함수타입
 function qqq(a: number, b: number): number {
@@ -52,5 +52,5 @@ interface a {
 }
 
 const aaaaa: a = {
-  a: 'sss',
+  a: "sss",
 };
